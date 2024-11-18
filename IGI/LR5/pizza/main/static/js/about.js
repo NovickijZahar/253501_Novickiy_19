75fbbec282c56ccaa994e454b34ba847fabdc26d
Zahar <zahar210200@gmail.com>
2024-11-18 23:18:35 +0300

    removing useless code

--- IGI/LR5/pizza/main/static/js/about.js
+++
@@ -1,9 +0,0 @@
-const mountainLeft = document.querySelector('#mountain_left');
-const mountainRight = document.querySelector('#mountain_right');
-
-window.addEventListener('scroll',()=>{
-    let value = scrollY;
-    mountainLeft.style.left = `-${value/0.7}px`
-    mountainRight.style.left = `${value/0.7}px`
-});
-
