231aee2a45a9750786f22f814d3390469f1bbe18
Zahar <zahar210200@gmail.com>
2024-11-18 23:12:48 +0300

    completed

--- IGI/LR5/pizza/main/static/js/styles.js
+++ IGI/LR5/pizza/main/static/js/styles.js
@@ -1,18 +1,23 @@
 const toggleControls = document.getElementById('toggleControls');
 const controls = document.getElementById('controls');
 
+
+
 toggleControls.addEventListener('change', () => {
-    controls.style.display = toggleControls.checked ? 'block' : 'none';
+    let div = document.getElementById('controls');
+    const elem = document.createElement('label');
+    div.appendChild(elem);
+    //controls.style.display = toggleControls.checked ? 'block' : 'none';
 });
 
-document.getElementById('fontSize').addEventListener('change', function() {
-    document.body.style.fontSize = this.value;
-});
+// document.getElementById('fontSize').addEventListener('change', function() {
+//     document.body.style.fontSize = this.value;
+// });
 
-document.getElementById('textColor').addEventListener('input', function() {
-    document.body.style.color = this.value;
-});
+// document.getElementById('textColor').addEventListener('input', function() {
+//     document.body.style.color = this.value;
+// });
 
-document.getElementById('bgColor').addEventListener('input', function() {
-    document.body.style.backgroundColor = this.value;
-});
+// document.getElementById('bgColor').addEventListener('input', function() {
+//     document.body.style.backgroundColor = this.value;
+// });
